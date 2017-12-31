548c2dd6101bd1cd6e44de56f7d650e3b28561a4
Kássio Guimarães <kassioguimaraes@protonmail.com>
2017-12-31 03:56:51 -0300

    even lighter colors

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#ffff7f";
+static const char col_cyan[]        = "#ffff9f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_cyan, col_gray1, col_gray2 },
