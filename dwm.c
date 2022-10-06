b8007a300b3a383472588b406405e91df6b7da2a
guimaraes <kassioguimaraes@protonmail.com>
2022-10-06 18:54:34 -0300

    allmon

--- dwm.c
+++ dwm.c
@@ -2305,9 +2305,12 @@ updatesizehints(Client *c)
 void
 updatestatus(void)
 {
+    Monitor* m;
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+	//drawbar(selmon);
+	for(m = mons; m; m = m->next)
+		drawbar(m);
 }
 
 void
